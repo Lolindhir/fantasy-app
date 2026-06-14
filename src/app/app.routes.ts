f9a716ae8af53757f2fe0ef4bc4b5ccdfba9e090
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-06-14 21:13:35 +0200

    Register league activity route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { OverviewComponent } from './overview/overview';
 import { TeamListComponent } from './team-list/team-list';
 import { PlayersPageComponent } from './players-page/players-page';
+import { LeagueActivityComponent } from './league-activity/league-activity';
 import { TradeSimulatorComponent } from './trade-simulator/trade-simulator';
 import { AboutComponent } from './about/about';
 
@@ -9,6 +10,7 @@ export const routes: Routes = [
   { path: '', component: OverviewComponent }, // aktuelle Startseite
   { path: 'teams', component: TeamListComponent },
   { path: 'players', component: PlayersPageComponent },
+  { path: 'league-activity', component: LeagueActivityComponent },
   { path: 'trade', component: TradeSimulatorComponent },
   { path: 'handbook', component: AboutComponent },
 
