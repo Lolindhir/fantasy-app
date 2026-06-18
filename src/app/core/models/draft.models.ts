08a1699dd63ad1b6ba36e9c00ac859399e7a6259
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-06-18 05:53:45 +0200

    Extract draft models to core models

---
+++ src/app/core/models/draft.models.ts
@@ -0,0 +1,60 @@
+export interface RawDraft {
+  LeagueID: string;
+  DraftKey: string;
+  DisplayDraftKey: string;
+  DisplayAbrDraftKey: string;
+  Season: string;
+  DraftType: string;
+  DisplayDraftType: string;
+  DraftNo: number;
+  DraftSource: string;
+  SleeperDraftID: string | null;
+  SleeperStatus: string | null;
+  Status: string;
+  DisplayStatus: string;
+  PickSource: string;
+  OrderSource: string;
+  OrderMode: string;
+  Settings: DraftSettings;
+  Picks: DraftPick[];
+}
+
+export interface DraftSettings {
+  Rounds: number;
+  Teams: number;
+  Type: string;
+}
+
+export interface DraftPickTradeHistoryEntry {
+  TransactionID: string;
+  Source: string;
+  CreatedAt: number;
+  CreatedDate: string;
+  DraftSource: string;
+  PreviousOwnerRosterID: number;
+  NewOwnerRosterID: number;
+}
+
+export interface DraftPick {
+  PickKey: string;
+  LeagueID: string;
+  DraftKey: string;
+  Season: string;
+  DraftType: string;
+  Round: number;
+  PositionInRound: number | null;
+  OverallPick: number | null;
+  DisplayPick: string;
+  OriginalOwnerRosterID: number;
+  CurrentOwnerRosterID: number;
+  WasTraded: boolean;
+  IsCurrentlyTraded: boolean;
+  TradeSource: string | null;
+  TradeHistory: DraftPickTradeHistoryEntry[];
+  PlayerID: string | null;
+  PlayerName: string | null;
+  Status: string;
+  SleeperPickNo: number | null;
+  SleeperPickedBy: string | null;
+  Draft?: RawDraft;
+}
