079a2c05cbc3346af2c030dd2eaba5b5fe7b88e7
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-06-20 10:07:42 +0200

    Add current drafts tab component

---
+++ src/app/features/drafts/tabs/current-drafts/current-drafts-tab.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { SharedMaterialImports } from '../../../../shared/shared-material-imports';
+import type { DraftViewModel } from '../../models/drafts-view.models';
+
+@Component({
+  selector: 'app-current-drafts-tab',
+  standalone: true,
+  imports: [
+    CommonModule,
+    SharedMaterialImports
+  ],
+  templateUrl: './current-drafts-tab.html',
+  styleUrl: './current-drafts-tab.scss'
+})
+export class CurrentDraftsTabComponent {
+  @Input({ required: true }) drafts: DraftViewModel[] = [];
+}
