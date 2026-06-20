e3482da47ceef2f13c120ca295849d904c1566a6
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-06-20 06:47:04 +0200

    Update DataService import in league activity

--- src/app/features/league-activity/league-activity.ts
+++ src/app/features/league-activity/league-activity.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { map } from 'rxjs/operators';
-import { DataService } from '../../services/data-service';
+import { DataService } from '../../core/services/data.service';
 import type { DraftPick, FantasyTeam, League, RawDraft } from '../../core/models/fantasy.models';
 import { SharedMaterialImports } from '../../shared/shared-material-imports';
 
