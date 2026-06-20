9b2b6f33acb40c866ea2802050c9dd76eac4ccee
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-06-20 06:45:16 +0200

    Update DataService import in team list

--- src/app/features/teams/team-list/team-list.ts
+++ src/app/features/teams/team-list/team-list.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../../../services/data-service';
+import { DataService } from '../../../core/services/data.service';
 import type { Player } from '../../../core/models/fantasy.models';
 import { CommonModule } from '@angular/common';
 import { ViewEncapsulation } from '@angular/core';
