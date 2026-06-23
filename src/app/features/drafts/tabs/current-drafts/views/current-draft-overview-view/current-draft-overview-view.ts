eda69922baa2b0394b1d79ea24b00edd8ffe9d0a
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-06-23 07:22:25 +0200

    Use position colors in draft overview

--- src/app/features/drafts/tabs/current-drafts/views/current-draft-overview-view/current-draft-overview-view.ts
+++ src/app/features/drafts/tabs/current-drafts/views/current-draft-overview-view/current-draft-overview-view.ts
@@ -4,12 +4,13 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatMenuModule } from '@angular/material/menu';
 import type { Player } from '../../../../../../core/models/fantasy.models';
 import { PlayerDetailDialogComponent } from '../../../../../../shared/components/player-detail-dialog/player-detail-dialog';
+import { PositionColorPipe } from '../../../../../../shared/pipes/position-color.pipe';
 import type { DraftViewModel } from '../../../../models/drafts-view.models';
 
 @Component({
   selector: 'app-current-draft-overview-view',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatMenuModule],
+  imports: [CommonModule, MatDialogModule, MatMenuModule, PositionColorPipe],
   templateUrl: './current-draft-overview-view.html',
   styleUrl: './current-draft-overview-view.scss'
 })
