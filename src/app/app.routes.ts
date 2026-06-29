749dd5623bd24eb42d227216e980bd0868d51265
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-06-29 13:01:02 +0200

    Add standings route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { OverviewComponent } from './features/overview/overview';
 import { TeamListComponent } from './features/teams/team-list/team-list';
+import { StandingsPageComponent } from './features/standings/standings-page';
 import { PlayersPageComponent } from './features/players/players-page/players-page';
 import { DraftsPageComponent } from './features/drafts/drafts-page';
 import { LeagueActivityComponent } from './features/league-activity/league-activity';
@@ -10,6 +11,7 @@ import { AboutComponent } from './features/handbook/about';
 export const routes: Routes = [
   { path: '', component: OverviewComponent },
   { path: 'teams', component: TeamListComponent },
+  { path: 'standings', component: StandingsPageComponent },
   { path: 'players', component: PlayersPageComponent },
   { path: 'drafts', component: DraftsPageComponent },
   { path: 'moves', component: LeagueActivityComponent },
