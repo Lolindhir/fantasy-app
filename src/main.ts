eebce7c8c7e1a25fe2f4b180604f7ff980302a3a
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-07-22 08:01:19 +0200

    Register JSON cache-busting interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  jsonDataCacheBustingInterceptor
+} from './app/core/interceptors/json-data-cache-busting.interceptor';
 
 bootstrapApplication(App, {
   ...appConfig,
   providers: [
     ...(appConfig.providers ?? []),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([jsonDataCacheBustingInterceptor])
+    )
   ]
 })
   .catch((err) => console.error(err));
