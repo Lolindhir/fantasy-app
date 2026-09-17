768cee01daf0c1d5e6779f2ecf8be6bf8765430e
Lolindhir <88049173+Lolindhir@users.noreply.github.com>
2026-09-17 05:30:03 +0200

    Compact Must Watch metadata row (#573)
    Fixes #572

--- src/app/testing/overview-weekly-dashboard.karma.spec.ts
+++ src/app/testing/overview-weekly-dashboard.karma.spec.ts
@@ -1,2 +1,3 @@
 import '../shared/utils/overview-weekly-dashboard.util.spec';
 import '../shared/components/league-matchups/league-weekly-dashboard.spec';
+import '../shared/components/league-matchups/league-must-watch-metadata.spec';
